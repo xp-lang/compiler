3308220508e75a33b3ca74aeb2497c04efb17ef0
Timm Friebe <thekid@thekid.de>
2012-04-21 16:19:30 +0200

    - Add tests for hex addition oddities # See http://me.veekun.com/blog/2012/04/09/php-a-fractal-of-bad-design/#numbers # See PHP Bug #61095 # See PHP Bug #61095 # XP Language does not expose these bugs

--- compiler/src/test/php/net/xp_lang/tests/syntax/xp/NumbersTest.class.php
+++ compiler/src/test/php/net/xp_lang/tests/syntax/xp/NumbersTest.class.php
@@ -47,5 +47,103 @@
     public function illegalHexMissingAfterX() {
       $this->parse('0x');
     }
+
+    /**
+     * Test "0x0+1"
+     *
+     */
+    #[@test]
+    public function php_bug_61256_0x0_plus_1() {
+      $this->assertEquals(
+        array(new BinaryOpNode(array(
+          'lhs' => new HexNode('0x0'), 
+          'rhs' => new IntegerNode('1'), 
+          'op'  => '+'
+        ))),
+        $this->parse('0x0+1;')
+      );
+    }
+
+    /**
+     * Test "0x0-1"
+     *
+     */
+    #[@test]
+    public function php_bug_61256_0x0_minus_1() {
+      $this->assertEquals(
+        array(new BinaryOpNode(array(
+          'lhs' => new HexNode('0x0'), 
+          'rhs' => new IntegerNode('1'), 
+          'op'  => '-'
+        ))),
+        $this->parse('0x0-1;')
+      );
+    }
+
+    /**
+     * Test "0x00+2"
+     *
+     */
+    #[@test]
+    public function php_bug_61095_0x00_plus_2() {
+      $this->assertEquals(
+        array(new BinaryOpNode(array(
+          'lhs' => new HexNode('0x00'), 
+          'rhs' => new IntegerNode('2'), 
+          'op'  => '+'
+        ))),
+        $this->parse('0x00+2;')
+      );
+    }
+
+    /**
+     * Test "0x00+0x02"
+     *
+     */
+    #[@test]
+    public function php_bug_61095_0x00_plus_0x02() {
+      $this->assertEquals(
+        array(new BinaryOpNode(array(
+          'lhs' => new HexNode('0x00'), 
+          'rhs' => new HexNode('0x02'), 
+          'op'  => '+'
+        ))),
+        $this->parse('0x00+0x02;')
+      );
+    }
+
+    /**
+     * Test "0x0+ 2"
+     *
+     * @see   http://me.veekun.com/blog/2012/04/09/php-a-fractal-of-bad-design/#numbers
+     */
+    #[@test]
+    public function hex_add_0x0_plus_2_with_space() {
+      $this->assertEquals(
+        array(new BinaryOpNode(array(
+          'lhs' => new HexNode('0x0'), 
+          'rhs' => new IntegerNode('2'), 
+          'op'  => '+'
+        ))),
+        $this->parse('0x0+ 2;')
+      );
+    }
+
+    /**
+     * Test "0x0+2"
+     *
+     * @see   http://me.veekun.com/blog/2012/04/09/php-a-fractal-of-bad-design/#numbers
+     */
+    #[@test]
+    public function hex_add_0x0_plus_2() {
+      $this->assertEquals(
+        array(new BinaryOpNode(array(
+          'lhs' => new HexNode('0x0'), 
+          'rhs' => new IntegerNode('2'), 
+          'op'  => '+'
+        ))),
+        $this->parse('0x0+2;')
+      );
+    }
   }
 ?>
