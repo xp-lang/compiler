6fb2301d27fda806dccbbe7a04f91b5edbc37c7a
Timm Friebe <thekid@thekid.de>
2012-04-25 09:46:32 +0200

    - Implement compact method syntax, "-> <e>" as shorthand for "{ return <e> }" # See xp-framework/rfc#241

---
+++ compiler/src/test/php/net/xp_lang/tests/syntax/xp/CompactSyntaxTest.class.php
@@ -0,0 +1,55 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('net.xp_lang.tests.syntax.xp.ParserTestCase');
+
+  /**
+   * TestCase
+   *
+   * @see   https://github.com/xp-framework/rfc/issues/241
+   */
+  class CompactSyntaxTest extends ParserTestCase {
+
+    /**
+     * Parse method source and return statements inside this method.
+     *
+     * @param   string src
+     * @return  xp.compiler.Node[]
+     */
+    protected function parse($src) {
+      try {
+        return create(new xp·compiler·syntax·xp·Parser())->parse(new xp·compiler·syntax·xp·Lexer(
+          $src, 
+          '<string:'.$this->name.'>'
+        ))->declaration->body;
+      } catch (ParseException $e) {
+        throw $e->getCause();
+      }
+    }
+  
+    /**
+     * Test "-> (expr)" as shorthand for "{ return (expr); }"
+     *
+     */
+    #[@test]
+    public function compact_return() {
+      $this->assertEquals(array(new MethodNode(array(
+        'modifiers'  => MODIFIER_PUBLIC,
+        'annotations'=> NULL,
+        'name'       => 'toString',
+        'returns'    => new TypeName('string'),
+        'parameters' => NULL,
+        'throws'     => NULL,
+        'body'       => array(
+          new ReturnNode(new StringNode('null'))
+        ),
+        'extension'  => NULL
+      ))), $this->parse('class Null { 
+        public string toString() -> "null";
+      }'));
+    }
+  }
+?>
