ae06ec802611ac1d8e29fbef1510984c6a3f7d86
Timm Friebe <thekid@thekid.de>
2012-06-07 17:12:15 +0200

    - Initial release: Tests with and without names

---
+++ compiler/src/test/php/net/xp_lang/tests/execution/source/NamedArgsTest.class.php
@@ -0,0 +1,60 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  $package= 'net.xp_lang.tests.execution.source';
+
+  uses('net.xp_lang.tests.execution.source.ExecutionTest');
+
+  /**
+   * Tests named arguments
+   *
+   */
+  class net·xp_lang·tests·execution·source·NamedArgsTest extends ExecutionTest {
+
+    /**
+     * Test passing arguments
+     *
+     * @param   string arguments
+     * @return  [:var]
+     */
+    protected function pass($arguments) {
+      $fixture= $this->define('class', $this->name, NULL, '{
+        public [:var] fixture(string $url= null, [:string] $params= [:], [:string] $headers= [:]) {
+          return [ url: $url, params: $params, headers: $headers];
+        }
+        
+        public [:var] run() {
+          return $this.fixture('.$arguments.');
+        }
+      }');
+      return $fixture->newInstance()->run();
+    }
+
+    /**
+     * Test 
+     *
+     */
+    #[@test]
+    public function pass_all_without_names() {
+      $this->assertEquals(
+        array('url' => '/', 'params' => array('a' => 'b'), 'headers' => array('X' => 'Y')),
+        $this->pass('"/", [ a: "b" ], [ X: "Y"]')
+      );
+    }
+
+    /**
+     * Test 
+     *
+     */
+    #[@test]
+    public function pass_all_with_names() {
+      $this->assertEquals(
+        array('url' => '/', 'params' => array('a' => 'b'), 'headers' => array('X' => 'Y')),
+        $this->pass('url: "/", params: [ a: "b" ], headers: [ X: "Y"]')
+      );
+    }
+  }
+?>
