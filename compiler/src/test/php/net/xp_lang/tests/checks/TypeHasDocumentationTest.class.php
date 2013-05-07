9acecbe7ba0534f89c858440d8efafcdc299bd1b
Timm Friebe <thekid@thekid.de>
2013-05-07 21:50:29 +0200

    - Convert to namespaces, part 2: Namespace net\xp_lang\tests\checks

--- compiler/src/test/php/net/xp_lang/tests/checks/TypeHasDocumentationTest.class.php
+++ compiler/src/test/php/net/xp_lang/tests/checks/TypeHasDocumentationTest.class.php
@@ -1,136 +1,129 @@
-<?php
-/* This class is part of the XP framework
- *
- * $Id$ 
- */
+<?php namespace net\xp_lang\tests\checks;
 
-  uses(
-    'unittest.TestCase',
-    'xp.compiler.checks.TypeHasDocumentation',
-    'xp.compiler.ast.InterfaceNode',
-    'xp.compiler.ast.EnumNode',
-    'xp.compiler.ast.ClassNode',
-    'xp.compiler.types.CompilationUnitScope'
-  );
+use xp\compiler\checks\TypeHasDocumentation;
+use xp\compiler\types\CompilationUnitScope;
+use xp\compiler\types\TypeName;
+use xp\compiler\ast\ClassNode;
+use xp\compiler\ast\InterfaceNode;
+use xp\compiler\ast\EnumNode;
+
+/**
+ * TestCase
+ *
+ * @see      xp://xp.compiler.checks.TypeHasDocumentation
+ */
+class TypeHasDocumentationTest extends \unittest\TestCase {
+  protected $fixture= null;
+  protected $scope= null;
 
   /**
-   * TestCase
+   * Sets up test case
    *
-   * @see      xp://xp.compiler.checks.TypeHasDocumentation
    */
-  class TypeHasDocumentationTest extends TestCase {
-    protected $fixture= NULL;
-    protected $scope= NULL;
-  
-    /**
-     * Sets up test case
-     *
-     */
-    public function setUp() {
-      $this->fixture= new TypeHasDocumentation();
-      $this->scope= new CompilationUnitScope();
-    }
-    
-    /**
-     * Test interfaces
-     *
-     */
-    #[@test]
-    public function interfaceWithoutApidoc() {
-      $this->assertEquals(
-        array('D201', 'No api doc for type Runnable'), 
-        $this->fixture->verify(new InterfaceNode(MODIFIER_PUBLIC, array(), new TypeName('Runnable')), $this->scope)
-      );
-    }
-
-    /**
-     * Test interfaces
-     *
-     */
-    #[@test]
-    public function interfaceWithApidoc() {
-      $i= new InterfaceNode(MODIFIER_PUBLIC, array(), new TypeName('Runnable'));
-      $i->comment= 'Comment';
-      $this->assertNull($this->fixture->verify($i, $this->scope));
-    }
-
-    /**
-     * Test classes
-     *
-     */
-    #[@test]
-    public function syntheticInterface() {
-      $i= new InterfaceNode(MODIFIER_PUBLIC, array(), new TypeName('Runnable'));
-      $i->synthetic= TRUE;
-      $this->assertNull($this->fixture->verify($i, $this->scope));
-    }
-
-    /**
-     * Test classes
-     *
-     */
-    #[@test]
-    public function classWithoutApidoc() {
-      $this->assertEquals(
-        array('D201', 'No api doc for type Runner'), 
-        $this->fixture->verify(new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner')), $this->scope)
-      );
-    }
-
-    /**
-     * Test classes
-     *
-     */
-    #[@test]
-    public function classWithApidoc() {
-      $c= new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner'));
-      $c->comment= 'Comment';
-      $this->assertNull($this->fixture->verify($c, $this->scope));
-    }
-
-    /**
-     * Test classes
-     *
-     */
-    #[@test]
-    public function syntheticClass() {
-      $c= new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner'));
-      $c->synthetic= TRUE;
-      $this->assertNull($this->fixture->verify($c, $this->scope));
-    }
-
-    /**
-     * Test enums
-     *
-     */
-    #[@test]
-    public function enumWithoutApidoc() {
-      $this->assertEquals(
-        array('D201', 'No api doc for type Runners'), 
-        $this->fixture->verify(new EnumNode(MODIFIER_PUBLIC, array(), new TypeName('Runners')), $this->scope)
-      );
-    }
-
-    /**
-     * Test classes
-     *
-     */
-    #[@test]
-    public function enumWithApidoc() {
-      $e= new EnumNode(MODIFIER_PUBLIC, array(), new TypeName('Runners'));
-      $e->comment= 'Comment';
-      $this->assertNull($this->fixture->verify($e, $this->scope));
-    }
-
-    /**
-     * Test classes
-     *
-     */
-    #[@test]
-    public function syntheticEnums() {
-      $e= new EnumNode(MODIFIER_PUBLIC, array(), new TypeName('Runners'));
-      $e->synthetic= TRUE;
-      $this->assertNull($this->fixture->verify($e, $this->scope));
-    }
+  public function setUp() {
+    $this->fixture= new TypeHasDocumentation();
+    $this->scope= new CompilationUnitScope();
   }
-?>
+  
+  /**
+   * Test interfaces
+   *
+   */
+  #[@test]
+  public function interfaceWithoutApidoc() {
+    $this->assertEquals(
+      array('D201', 'No api doc for type Runnable'), 
+      $this->fixture->verify(new InterfaceNode(MODIFIER_PUBLIC, array(), new TypeName('Runnable')), $this->scope)
+    );
+  }
+
+  /**
+   * Test interfaces
+   *
+   */
+  #[@test]
+  public function interfaceWithApidoc() {
+    $i= new InterfaceNode(MODIFIER_PUBLIC, array(), new TypeName('Runnable'));
+    $i->comment= 'Comment';
+    $this->assertNull($this->fixture->verify($i, $this->scope));
+  }
+
+  /**
+   * Test classes
+   *
+   */
+  #[@test]
+  public function syntheticInterface() {
+    $i= new InterfaceNode(MODIFIER_PUBLIC, array(), new TypeName('Runnable'));
+    $i->synthetic= true;
+    $this->assertNull($this->fixture->verify($i, $this->scope));
+  }
+
+  /**
+   * Test classes
+   *
+   */
+  #[@test]
+  public function classWithoutApidoc() {
+    $this->assertEquals(
+      array('D201', 'No api doc for type Runner'), 
+      $this->fixture->verify(new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner')), $this->scope)
+    );
+  }
+
+  /**
+   * Test classes
+   *
+   */
+  #[@test]
+  public function classWithApidoc() {
+    $c= new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner'));
+    $c->comment= 'Comment';
+    $this->assertNull($this->fixture->verify($c, $this->scope));
+  }
+
+  /**
+   * Test classes
+   *
+   */
+  #[@test]
+  public function syntheticClass() {
+    $c= new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner'));
+    $c->synthetic= true;
+    $this->assertNull($this->fixture->verify($c, $this->scope));
+  }
+
+  /**
+   * Test enums
+   *
+   */
+  #[@test]
+  public function enumWithoutApidoc() {
+    $this->assertEquals(
+      array('D201', 'No api doc for type Runners'), 
+      $this->fixture->verify(new EnumNode(MODIFIER_PUBLIC, array(), new TypeName('Runners')), $this->scope)
+    );
+  }
+
+  /**
+   * Test classes
+   *
+   */
+  #[@test]
+  public function enumWithApidoc() {
+    $e= new EnumNode(MODIFIER_PUBLIC, array(), new TypeName('Runners'));
+    $e->comment= 'Comment';
+    $this->assertNull($this->fixture->verify($e, $this->scope));
+  }
+
+  /**
+   * Test classes
+   *
+   */
+  #[@test]
+  public function syntheticEnums() {
+    $e= new EnumNode(MODIFIER_PUBLIC, array(), new TypeName('Runners'));
+    $e->synthetic= true;
+    $this->assertNull($this->fixture->verify($e, $this->scope));
+  }
+}
--- compiler/src/test/php/net/xp_lang/tests/checks/TypeMemberHasDocumentationTest.class.php
+++ compiler/src/test/php/net/xp_lang/tests/checks/TypeMemberHasDocumentationTest.class.php
@@ -1,81 +1,78 @@
-<?php
-/* This class is part of the XP framework
- *
- * $Id$
- */
+<?php namespace net\xp_lang\tests\checks;
 
-  uses(
-    'unittest.TestCase',
-    'xp.compiler.checks.TypeMemberHasDocumentation',
-    'xp.compiler.ast.MethodNode',
-    'xp.compiler.ast.ClassNode',
-    'xp.compiler.types.TypeDeclarationScope'
-  );
+use xp\compiler\checks\TypeMemberHasDocumentation;
+use xp\compiler\types\TypeDeclarationScope;
+use xp\compiler\types\TypeName;
+use xp\compiler\ast\TypeDeclarationNode;
+use xp\compiler\ast\ClassNode;
+use xp\compiler\ast\InterfaceNode;
+use xp\compiler\ast\EnumNode;
+use xp\compiler\ast\RoutineNode;
+use xp\compiler\ast\MethodNode;
+
+/**
+ * TestCase
+ *
+ * @see      xp://xp.compiler.checks.TypeMemberHasDocumentation
+ */
+class TypeMemberHasDocumentationTest extends \unittest\TestCase {
+  protected $fixture= null;
 
   /**
-   * TestCase
+   * Sets up test case
    *
-   * @see      xp://xp.compiler.checks.TypeMemberHasDocumentation
    */
-  class TypeMemberHasDocumentationTest extends TestCase {
-    protected $fixture= NULL;
-  
-    /**
-     * Sets up test case
-     *
-     */
-    public function setUp() {
-      $this->fixture= new TypeMemberHasDocumentation();
-    }
-
-    /**
-     * Wrapper around verify
-     *
-     * @param   xp.compiler.ast.RoutineNode routine
-     * @param   xp.compiler.ast.TypeDeclarationNode type
-     * @return  var
-     */
-    protected function verify(RoutineNode $routine, TypeDeclarationNode $type) {
-      $scope= new TypeDeclarationScope();
-      $scope->declarations[0]= $type;
-      return $this->fixture->verify($routine, $scope);
-    }
-    
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function methodWithoutApidoc() {
-      $m= new MethodNode(array(
-        'name'        => 'run',
-        'modifiers'   => MODIFIER_ABSTRACT,
-        'returns'     => TypeName::$VOID,
-        'parameters'  => array(),
-        'body'        => array()
-      ));
-      $this->assertEquals(
-        array('D201', 'No api doc for member Runner::run'), 
-        $this->verify($m, new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner')))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function methodsInSyntheticClassesNotChecked() {
-      $c= new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Lambda··4b70075bd9164'));
-      $c->synthetic= TRUE;
-      $m= new MethodNode(array(
-        'name'        => 'run',
-        'modifiers'   => MODIFIER_ABSTRACT,
-        'returns'     => TypeName::$VOID,
-        'parameters'  => array(),
-        'body'        => array()
-      ));
-      $this->assertNull($this->verify($m, $c));
-    }
+  public function setUp() {
+    $this->fixture= new TypeMemberHasDocumentation();
   }
-?>
+
+  /**
+   * Wrapper around verify
+   *
+   * @param   xp.compiler.ast.RoutineNode routine
+   * @param   xp.compiler.ast.TypeDeclarationNode type
+   * @return  var
+   */
+  protected function verify(RoutineNode $routine, TypeDeclarationNode $type) {
+    $scope= new TypeDeclarationScope();
+    $scope->declarations[0]= $type;
+    return $this->fixture->verify($routine, $scope);
+  }
+  
+  /**
+   * Test
+   *
+   */
+  #[@test]
+  public function methodWithoutApidoc() {
+    $m= new MethodNode(array(
+      'name'        => 'run',
+      'modifiers'   => MODIFIER_ABSTRACT,
+      'returns'     => TypeName::$VOID,
+      'parameters'  => array(),
+      'body'        => array()
+    ));
+    $this->assertEquals(
+      array('D201', 'No api doc for member Runner::run'), 
+      $this->verify($m, new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Runner')))
+    );
+  }
+
+  /**
+   * Test
+   *
+   */
+  #[@test]
+  public function methodsInSyntheticClassesNotChecked() {
+    $c= new ClassNode(MODIFIER_PUBLIC, array(), new TypeName('Lambda··4b70075bd9164'));
+    $c->synthetic= true;
+    $m= new MethodNode(array(
+      'name'        => 'run',
+      'modifiers'   => MODIFIER_ABSTRACT,
+      'returns'     => TypeName::$VOID,
+      'parameters'  => array(),
+      'body'        => array()
+    ));
+    $this->assertNull($this->verify($m, $c));
+  }
+}
