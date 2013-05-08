c54e77ec34f57fb5a3db891a90bf02434af66d5d
Timm Friebe <thekid@thekid.de>
2013-05-08 10:25:04 +0200

    - Convert to namespaces, part 5: Namespace net\xp_lang\tests

--- compiler/src/test/php/net/xp_lang/tests/LocalsToMemberPromoterTest.class.php
+++ compiler/src/test/php/net/xp_lang/tests/LocalsToMemberPromoterTest.class.php
@@ -1,113 +1,104 @@
-<?php
-/* This class is part of the XP framework
- *
- * $Id$ 
- */
+<?php namespace net\xp_lang\tests;
 
-  uses(
-    'unittest.TestCase',
-    'xp.compiler.ast.StatementsNode',
-    'xp.compiler.ast.AssignmentNode',
-    'xp.compiler.ast.VariableNode',
-    'xp.compiler.ast.MemberAccessNode',
-    'xp.compiler.ast.IntegerNode',
-    'xp.compiler.ast.LocalsToMemberPromoter'
-  );
+use xp\compiler\ast\StatementsNode;
+use xp\compiler\ast\AssignmentNode;
+use xp\compiler\ast\VariableNode;
+use xp\compiler\ast\MemberAccessNode;
+use xp\compiler\ast\IntegerNode;
+
+/**
+ * TestCase
+ *
+ * @see      xp://xp.compiler.ast.LocalsToMemberPromoter
+ */
+class LocalsToMemberPromoterTest extends \unittest\TestCase {
+  protected $fixture= NULL;
 
   /**
-   * TestCase
+   * Creates fixture
    *
-   * @see      xp://xp.compiler.ast.LocalsToMemberPromoter
    */
-  class LocalsToMemberPromoterTest extends TestCase {
-    protected $fixture= NULL;
+  public function setUp() {
+    $this->fixture= new \xp\compiler\ast\LocalsToMemberPromoter();
+  }
   
-    /**
-     * Creates fixture
-     *
-     */
-    public function setUp() {
-      $this->fixture= new LocalsToMemberPromoter();
-    }
-    
-    /**
-     * Creates a member node
-     *
-     * @param   string name
-     * @return  xp.compiler.ast.MemberAccessNode
-     */
-    protected function memberNode($name) {
-      return new MemberAccessNode(new VariableNode('this'), $name);
-    }
+  /**
+   * Creates a member node
+   *
+   * @param   string name
+   * @return  xp.compiler.ast.MemberAccessNode
+   */
+  protected function memberNode($name) {
+    return new MemberAccessNode(new VariableNode('this'), $name);
+  }
 
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function basic() {
-      $assignment= new AssignmentNode(array(
-        'variable'      => new VariableNode('i'),
+  /**
+   * Test
+   *
+   */
+  #[@test]
+  public function basic() {
+    $assignment= new AssignmentNode(array(
+      'variable'      => new VariableNode('i'),
+      'expression'    => new IntegerNode('0'),
+      'op'            => '='
+    ));
+    $promoted= $this->fixture->promote($assignment);
+    $this->assertEquals(array('$i' => $this->memberNode('i')), $promoted['replaced']);
+    $this->assertEquals(
+      new AssignmentNode(array(
+        'variable'      => $this->memberNode('i'),
         'expression'    => new IntegerNode('0'),
         'op'            => '='
-      ));
-      $promoted= $this->fixture->promote($assignment);
-      $this->assertEquals(array('$i' => $this->memberNode('i')), $promoted['replaced']);
-      $this->assertEquals(
-        new AssignmentNode(array(
-          'variable'      => $this->memberNode('i'),
-          'expression'    => new IntegerNode('0'),
-          'op'            => '='
-        )),
-        $promoted['node']
-      );
-    }
+      )),
+      $promoted['node']
+    );
+  }
 
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function withExclusion() {
-      $assignment= new AssignmentNode(array(
-        'variable'      => new VariableNode('i'),
+  /**
+   * Test
+   *
+   */
+  #[@test]
+  public function withExclusion() {
+    $assignment= new AssignmentNode(array(
+      'variable'      => new VariableNode('i'),
+      'expression'    => new VariableNode('a'),
+      'op'            => '='
+    ));
+    $this->fixture->exclude('a');
+    $promoted= $this->fixture->promote($assignment);
+    $this->assertEquals(array('$i' => $this->memberNode('i')), $promoted['replaced']);
+    $this->assertEquals(
+      new AssignmentNode(array(
+        'variable'      => $this->memberNode('i'),
         'expression'    => new VariableNode('a'),
         'op'            => '='
-      ));
-      $this->fixture->exclude('a');
-      $promoted= $this->fixture->promote($assignment);
-      $this->assertEquals(array('$i' => $this->memberNode('i')), $promoted['replaced']);
-      $this->assertEquals(
-        new AssignmentNode(array(
-          'variable'      => $this->memberNode('i'),
-          'expression'    => new VariableNode('a'),
-          'op'            => '='
-        )),
-        $promoted['node']
-      );
-    }
+      )),
+      $promoted['node']
+    );
+  }
 
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function memberNodesArentTouched() {
-      $assignment= new AssignmentNode(array(
-        'variable'      => new VariableNode('i'),
+  /**
+   * Test
+   *
+   */
+  #[@test]
+  public function memberNodesArentTouched() {
+    $assignment= new AssignmentNode(array(
+      'variable'      => new VariableNode('i'),
+      'expression'    => $this->memberNode('i'),
+      'op'            => '='
+    ));
+    $promoted= $this->fixture->promote($assignment);
+    $this->assertEquals(array('$i' => $this->memberNode('i')), $promoted['replaced']);
+    $this->assertEquals(
+      new AssignmentNode(array(
+        'variable'      => $this->memberNode('i'),
         'expression'    => $this->memberNode('i'),
         'op'            => '='
-      ));
-      $promoted= $this->fixture->promote($assignment);
-      $this->assertEquals(array('$i' => $this->memberNode('i')), $promoted['replaced']);
-      $this->assertEquals(
-        new AssignmentNode(array(
-          'variable'      => $this->memberNode('i'),
-          'expression'    => $this->memberNode('i'),
-          'op'            => '='
-        )),
-        $promoted['node']
-      );
-    }
+      )),
+      $promoted['node']
+    );
   }
-?>
+}
