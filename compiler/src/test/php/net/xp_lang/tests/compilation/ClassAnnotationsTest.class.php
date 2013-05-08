eb417f93ec0b2214dd8d293775667c1e7bdbf690
Timm Friebe <thekid@thekid.de>
2013-05-08 09:31:15 +0200

    - Convert to namespaces, part 4: Namespace net\xp_lang\tests\compilation

--- compiler/src/test/php/net/xp_lang/tests/compilation/ClassAnnotationsTest.class.php
+++ compiler/src/test/php/net/xp_lang/tests/compilation/ClassAnnotationsTest.class.php
@@ -1,63 +1,54 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace net\xp_lang\tests\compilation;
+
+/**
+ * TestCase for class annotations
  *
- * $Id$ 
+ * @see   xp://net.xp_lang.tests.compilation.AnnotationsTest
  */
-
-  $package= 'net.xp_lang.tests.compilation';
-
-  uses('net.xp_lang.tests.compilation.AnnotationsTest');
+class ClassAnnotationsTest extends AnnotationsTest {
 
   /**
-   * TestCase for class annotations
+   * Test XPClass::getAnnotations() on compiled type
    *
-   * @see   xp://net.xp_lang.tests.compilation.AnnotationsTest
    */
-  class net·xp_lang·tests·compilation·ClassAnnotationsTest extends net·xp_lang·tests·compilation·AnnotationsTest {
-
-    /**
-     * Test XPClass::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function noAnnotations() {
-      $this->assertEquals(array(), $this->compile('class %s { }')->getAnnotations());
-    }
-
-    /**
-     * Test XPClass::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function simpleAnnotation() {
-      $this->assertEquals(
-        array('experimental' => NULL), 
-        $this->compile('[@experimental] class %s { }')->getAnnotations()
-      );
-    }
-
-    /**
-     * Test XPClass::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function annotationWithDefault() {
-      $this->assertEquals(
-        array('experimental' => 'beta'), 
-        $this->compile('[@experimental("beta")] class %s { }')->getAnnotations()
-      );
-    }
-
-    /**
-     * Test XPClass::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function annotationWithParams() {
-      $this->assertEquals(
-        array('experimental' => array('stages' => array('beta', 'RC'))), 
-        $this->compile('[@experimental(stages= ["beta", "RC"])] class %s { }')->getAnnotations()
-      );
-    }
+  #[@test]
+  public function noAnnotations() {
+    $this->assertEquals(array(), $this->compile('class %s { }')->getAnnotations());
   }
-?>
+
+  /**
+   * Test XPClass::getAnnotations() on compiled type
+   *
+   */
+  #[@test]
+  public function simpleAnnotation() {
+    $this->assertEquals(
+      array('experimental' => NULL), 
+      $this->compile('[@experimental] class %s { }')->getAnnotations()
+    );
+  }
+
+  /**
+   * Test XPClass::getAnnotations() on compiled type
+   *
+   */
+  #[@test]
+  public function annotationWithDefault() {
+    $this->assertEquals(
+      array('experimental' => 'beta'), 
+      $this->compile('[@experimental("beta")] class %s { }')->getAnnotations()
+    );
+  }
+
+  /**
+   * Test XPClass::getAnnotations() on compiled type
+   *
+   */
+  #[@test]
+  public function annotationWithParams() {
+    $this->assertEquals(
+      array('experimental' => array('stages' => array('beta', 'RC'))), 
+      $this->compile('[@experimental(stages= ["beta", "RC"])] class %s { }')->getAnnotations()
+    );
+  }
+}
--- compiler/src/test/php/net/xp_lang/tests/compilation/ConstructorAnnotationsTest.class.php
+++ compiler/src/test/php/net/xp_lang/tests/compilation/ConstructorAnnotationsTest.class.php
@@ -1,63 +1,54 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace net\xp_lang\tests\compilation;
+
+/**
+ * TestCase for constructor annotations
  *
- * $Id$ 
+ * @see   xp://net.xp_lang.tests.compilation.AnnotationsTest
  */
-
-  $package= 'net.xp_lang.tests.compilation';
-
-  uses('net.xp_lang.tests.compilation.AnnotationsTest');
+class ConstructorAnnotationsTest extends AnnotationsTest {
 
   /**
-   * TestCase for constructor annotations
+   * Test Method::getAnnotations() on compiled type
    *
-   * @see   xp://net.xp_lang.tests.compilation.AnnotationsTest
    */
-  class net·xp_lang·tests·compilation·ConstructorAnnotationsTest extends net·xp_lang·tests·compilation·AnnotationsTest {
-
-    /**
-     * Test Method::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function noAnnotations() {
-      $this->assertEquals(array(), $this->compile('class %s { __construct() { } }')->getConstructor()->getAnnotations());
-    }
-
-    /**
-     * Test Method::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function simpleAnnotation() {
-      $this->assertEquals(
-        array('experimental' => NULL), 
-        $this->compile('class %s { [@experimental] __construct() { } }')->getConstructor()->getAnnotations()
-      );
-    }
-
-    /**
-     * Test Method::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function annotationWithDefault() {
-      $this->assertEquals(
-        array('experimental' => 'beta'), 
-        $this->compile('class %s { [@experimental("beta")] __construct() { } }')->getConstructor()->getAnnotations()
-      );
-    }
-
-    /**
-     * Test Method::getAnnotations() on compiled type
-     *
-     */
-    #[@test]
-    public function annotationWithParams() {
-      $this->assertEquals(
-        array('experimental' => array('stages' => array('beta', 'RC'))), 
-        $this->compile('class %s { [@experimental(stages= ["beta", "RC"])] __construct() { } }')->getConstructor()->getAnnotations()
-      );
-    }
+  #[@test]
+  public function noAnnotations() {
+    $this->assertEquals(array(), $this->compile('class %s { __construct() { } }')->getConstructor()->getAnnotations());
   }
-?>
+
+  /**
+   * Test Method::getAnnotations() on compiled type
+   *
+   */
+  #[@test]
+  public function simpleAnnotation() {
+    $this->assertEquals(
+      array('experimental' => NULL), 
+      $this->compile('class %s { [@experimental] __construct() { } }')->getConstructor()->getAnnotations()
+    );
+  }
+
+  /**
+   * Test Method::getAnnotations() on compiled type
+   *
+   */
+  #[@test]
+  public function annotationWithDefault() {
+    $this->assertEquals(
+      array('experimental' => 'beta'), 
+      $this->compile('class %s { [@experimental("beta")] __construct() { } }')->getConstructor()->getAnnotations()
+    );
+  }
+
+  /**
+   * Test Method::getAnnotations() on compiled type
+   *
+   */
+  #[@test]
+  public function annotationWithParams() {
+    $this->assertEquals(
+      array('experimental' => array('stages' => array('beta', 'RC'))), 
+      $this->compile('class %s { [@experimental(stages= ["beta", "RC"])] __construct() { } }')->getConstructor()->getAnnotations()
+    );
+  }
+}
