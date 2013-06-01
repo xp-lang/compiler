186229fa56d36e39b5aa324563ec13d32215f7f9
Timm Friebe <thekid@thekid.de>
2013-06-01 15:55:57 +0200

    - Put into namespace

--- jit/init.php
+++ jit/init.php
@@ -1,13 +1,16 @@
-<?php
+<?php namespace xp\compiler;
+
+use lang\ClassLoader;
+
 $f= function() use(&$f) {
   if (class_exists('ClassLoader', false)) {
 
     // Ensure delegates are set up. Unfortunately, the static initializer will 
     // be invoked twice - although with no effect, it's unnecessary nevertheless:/
-    \lang\ClassLoader::__static();
-    \lang\ClassLoader::registerLoader(\xp\compiler\JitClassLoader::instanceFor(''), true);
+    ClassLoader::__static();
+    ClassLoader::registerLoader(JitClassLoader::instanceFor(''), true);
   } else {
-    xp::$cli[]= $f;
+    \xp::$cli[]= $f;
   }
 };
-xp::$cli[]= $f;
+$f();
