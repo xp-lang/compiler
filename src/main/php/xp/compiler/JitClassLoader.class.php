f54f80b2e773ae4328e328ed403e717bb707e803
Timm Friebe <thekid@thekid.de>
2013-06-01 13:53:15 +0200

    - First working version

--- src/main/php/xp/compiler/JitClassLoader.class.php
+++ src/main/php/xp/compiler/JitClassLoader.class.php
@@ -1,12 +1,18 @@
 <?php namespace xp\compiler;
 
+use xp\compiler\types\TypeName;
+use xp\compiler\types\TaskScope;
+use xp\compiler\io\FileManager;
 use xp\compiler\io\ClassLoaderSource;
+use xp\compiler\task\CompilationTask;
+use xp\compiler\diagnostic\NullDiagnosticListener;
+use xp\compiler\Syntax;
 
 class JitClassLoader extends \lang\Object implements \lang\IClassLoader {
   protected $source= array();
 
   static function __static() {
-    \lang\XPClass::forName('xp.compiler.Syntax');   // Ensure Syntax class is loaded
+    uses('xp.compiler.Syntax', 'io.File');   // HACK: Ensure Syntax and File classes are loaded
   }
 
   /**
@@ -69,15 +75,47 @@ class JitClassLoader extends \lang\Object implements \lang\IClassLoader {
     return array(); // TBI
   }
 
+  /**
+   * Loads a class
+   *
+   * @param  string $class
+   * @return lang.XPClass
+   * @throws lang.ClassLoadingException
+   */
   public function loadClass($class) {
     return new \lang\XPClass($this->loadClass0($class));
   }
+
+  /**
+   * Compiles a class if necessary
+   *
+   * @param  string $class
+   * @return string
+   * @throws lang.ClassLoadingException
+   */
   public function loadClass0($class) {
     if (null === ($source= $this->locateSource($class))) {
       throw new \lang\ClassNotFoundException($class);  
     }
-    fputs(STDERR, $source->toString()."\n");
-    throw new \lang\ClassNotFoundException($class);
+
+    // Parse, then emit source
+    // fputs(STDERR, "COMPILE ".$source->toString()."...\n");
+    $emitter= new \xp\compiler\emit\source\Emitter();
+    $scope= new TaskScope(new CompilationTask(
+      $source,
+      new NullDiagnosticListener(),
+      new FileManager(),
+      $emitter
+    ));
+    try {
+      $r= $emitter->emit($source->getSyntax()->parse($source->getInputStream()), $scope);
+    } catch (\lang\Throwable $e) {
+      throw new \lang\ClassLoadingException('Cannot compile '.$source->getURI(), $e);
+    }
+
+    // Define type
+    $r->executeWith(array());
+    return $r->type()->literal();
   }
   public function getResource($string) {
     throw new \lang\ElementNotFoundException($string);
@@ -86,6 +124,6 @@ class JitClassLoader extends \lang\Object implements \lang\IClassLoader {
     throw new \lang\ElementNotFoundException($string);
   }
   public function instanceId() {
-    return "jit";
+    return 'jit';
   }
 }
