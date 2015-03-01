313f940a13cb313a30f59db9b4be842456f51dc4
Timm Friebe <thekid@thekid.de>
2015-03-01 11:22:33 +0100

    Migrate to new coding standards

--- src/test/php/net/xp_lang/tests/RunnerTest.class.php
+++ src/test/php/net/xp_lang/tests/RunnerTest.class.php
@@ -1,8 +1,12 @@
 <?php namespace net\xp_lang\tests;
 
 use xp\compiler\Runner;
+use xp\compiler\ast\ParseTree;
+use xp\compiler\types\Scope;
 use util\cmd\Console;
 use io\streams\MemoryOutputStream;
+use lang\ClassLoader;
+use lang\Throwable;
 
 class RunnerTest extends \unittest\TestCase {
 
@@ -12,15 +16,15 @@ class RunnerTest extends \unittest\TestCase {
    * @param  string[] $args
    * @return [:var]
    */
-  protected function run($args= array()) {
-    $saved= array('out' => Console::$out->getStream(), 'err' => Console::$err->getStream());
+  protected function run($args= []) {
+    $saved= ['out' => Console::$out->getStream(), 'err' => Console::$err->getStream()];
     Console::$out->setStream($out= new MemoryOutputStream());
     Console::$err->setStream($err= new MemoryOutputStream());
 
     try {
       $exit= Runner::main($args);
-      $r= array('exit' => $exit, 'out' => $out->getBytes(), 'err' => $err->getBytes());
-    } catch (\lang\Throwable $t) {
+      $r= ['exit' => $exit, 'out' => $out->getBytes(), 'err' => $err->getBytes()];
+    } catch (Throwable $t) {
       // Fall through
     } ensure($t); {
       Console::$out->setStream($saved['out']);
@@ -44,22 +48,22 @@ class RunnerTest extends \unittest\TestCase {
   
   #[@test]
   public function run_without_parameters_shows_usage_on_stderr() {
-    $this->assertMatches('Usage:', this($this->run(), 'err'));
+    $this->assertMatches('Usage:', $this->run()['err']);
   }
 
   #[@test]
   public function run_without_parameters_yields_nonzero_exitcode() {
-    $this->assertEquals(2, this($this->run(), 'exit'));
+    $this->assertEquals(2, $this->run()['exit']);
   }
 
   #[@test]
   public function run_with_short_help_shows_usage_on_stderr() {
-    $this->assertMatches('Usage:', this($this->run(array('-?')), 'err'));
+    $this->assertMatches('Usage:', $this->run(['-?'])['err']);
   }
 
   #[@test]
   public function run_with_long_help_shows_usage_on_stderr() {
-    $this->assertMatches('Usage:', this($this->run(array('--help')), 'err'));
+    $this->assertMatches('Usage:', $this->run(['--help'])['err']);
   }
 
   #[@test, @values([
@@ -68,8 +72,8 @@ class RunnerTest extends \unittest\TestCase {
   #])]
   public function evaluate($source) {
     $this->assertEquals(
-      array('exit' => 0, 'out' => "Hello World\n", 'err' => ''),
-      $this->run(array('-e', 'xp', $source))
+      ['exit' => 0, 'out' => "Hello World\n", 'err' => ''],
+      $this->run(['-e', 'xp', $source])
     );
   }
 
@@ -81,21 +85,21 @@ class RunnerTest extends \unittest\TestCase {
   #])]
   public function write($source) {
     $this->assertEquals(
-      array('exit' => 0, 'out' => "Hello World\n", 'err' => ''),
-      $this->run(array('-w', 'xp', $source))
+      ['exit' => 0, 'out' => "Hello World\n", 'err' => ''],
+      $this->run(['-w', 'xp', $source])
     );
   }
 
   #[@test]
   public function syntax_error_yields_nonzero_exitcode() {
-    $this->assertEquals(1, this($this->run(array('-e', 'xp', '@syntax error@')), 'exit'));
+    $this->assertEquals(1, $this->run(['-e', 'xp', '@syntax error@'])['exit']);
   }
 
   #[@test]
   public function syntax_error_shows_error_on_stdout() {
     $this->assertMatches(
       'Syntax error at Command line argument',
-      this($this->run(array('-e', 'xp', '@syntax error@')), 'out')
+      $this->run(['-e', 'xp', '@syntax error@'])['out']
     );
   }
 
@@ -103,7 +107,7 @@ class RunnerTest extends \unittest\TestCase {
   public function select_v52_emitter() {
     $this->assertEquals(
       "Test\n",
-      this($this->run(array('-E', 'php5.2', '-w', 'xp', '"Test"')), 'out')
+      $this->run(['-E', 'php5.2', '-w', 'xp', '"Test"'])['out']
     );
   }
 
@@ -111,21 +115,21 @@ class RunnerTest extends \unittest\TestCase {
   public function select_v53_emitter() {
     $this->assertEquals(
       "Test\n",
-      this($this->run(array('-E', 'php5.3', '-w', 'xp', '"Test"')), 'out')
+      $this->run(['-E', 'php5.3', '-w', 'xp', '"Test"'])['out']
     );
   }
 
   #[@test]
   public function select_userdefined_emitter() {
-    \lang\ClassLoader::defineClass('xp.compiler.emit.test.Emitter', 'xp.compiler.emit.php.V53Emitter', array(), '{
-      public function emit(\xp\compiler\ast\ParseTree $tree, xp\compiler\types\Scope $scope) {
-        \util\cmd\Console::writeLine("Test emitter emitting...");
+    ClassLoader::defineClass('xp.compiler.emit.test.Emitter', 'xp.compiler.emit.php.V53Emitter', [], [
+      'emit' => function(ParseTree $tree, Scope $scope) {
+        Console::writeLine('Test emitter emitting...');
         return parent::emit($tree, $scope);
       }
-    }');
+    ]);
     $this->assertEquals(
       "Test emitter emitting...\nTest\n",
-      this($this->run(array('-E', 'test', '-w', 'xp', '"Test"')), 'out')
+      $this->run(['-E', 'test', '-w', 'xp', '"Test"'])['out']
     );
   }
 
@@ -133,7 +137,7 @@ class RunnerTest extends \unittest\TestCase {
   public function select_non_existant_emitter() {
     $this->assertMatches(
       'No emitter named "@non_existant@"',
-      this($this->run(array('-E', '@non_existant@', '-w', 'xp', '"Test"')), 'err')
+      $this->run(['-E', '@non_existant@', '-w', 'xp', '"Test"'])['err']
     );
   }
 
@@ -141,7 +145,7 @@ class RunnerTest extends \unittest\TestCase {
   public function select_non_existant_emitter_version() {
     $this->assertMatches(
       'No emitter named "php5.1"',
-      this($this->run(array('-E', 'php5.1', '-w', 'xp', '"Test"')), 'err')
+      $this->run(['-E', 'php5.1', '-w', 'xp', '"Test"'])['err']
     );
   }
 
@@ -149,18 +153,16 @@ class RunnerTest extends \unittest\TestCase {
   public function select_illegal_emitter() {
     $this->assertMatches(
       'Cannot use emitter named "php"',
-      this($this->run(array('-E', 'php', '-w', 'xp', '"Test"')), 'err')
+      $this->run(['-E', 'php', '-w', 'xp', '"Test"'])['err']
     );
   }
 
   #[@test]
   public function select_non_emitter() {
-    \lang\ClassLoader::defineClass('xp.compiler.emit.non.V1Emitter', 'lang.Object', array(), '{
-      // Empty
-    }');
+    ClassLoader::defineClass('xp.compiler.emit.non.V1Emitter', 'lang.Object', []);
     $this->assertMatches(
       'Not an emitter implementation',
-      this($this->run(array('-E', 'non1', '-w', 'xp', '"Test"')), 'err')
+      $this->run(['-E', 'non1', '-w', 'xp', '"Test"'])['err']
     );
   }
 }
