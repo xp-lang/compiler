8e115666320c98ae571e25bf69c968ad04baff9d
Timm Friebe <thekid@thekid.de>
2015-09-20 15:57:13 +0200

    Allow dereferencing constant arrays and strings
    See https://wiki.php.net/rfc/constdereference

--- src/test/php/net/xp_lang/tests/syntax/php/ChainingTest.class.php
+++ src/test/php/net/xp_lang/tests/syntax/php/ChainingTest.class.php
@@ -4,6 +4,7 @@ use xp\compiler\ast\MethodCallNode;
 use xp\compiler\ast\VariableNode;
 use xp\compiler\ast\InstanceCreationNode;
 use xp\compiler\ast\MemberAccessNode;
+use xp\compiler\ast\ArrayNode;
 use xp\compiler\ast\ArrayAccessNode;
 use xp\compiler\ast\IntegerNode;
 use xp\compiler\ast\StaticMethodCallNode;
@@ -17,7 +18,7 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function methodCall() {
     $this->assertEquals(
-      array(new MethodCallNode(new VariableNode('m'), 'invoke', array(new VariableNode('args')))),
+      [new MethodCallNode(new VariableNode('m'), 'invoke', [new VariableNode('args')])],
       $this->parse('$m->invoke($args);')
     );
   }
@@ -25,11 +26,11 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function chainedMethodCalls() {
     $this->assertEquals(
-      array(new MethodCallNode(
+      [new MethodCallNode(
         new MethodCallNode(new VariableNode('l'), 'withAppender', null),
         'debug',
         null
-      )),
+      )],
       $this->parse('$l->withAppender()->debug();')
     );
   }
@@ -37,14 +38,14 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function chainedAfterNew() {
     $this->assertEquals(
-      array(new MethodCallNode(
+      [new MethodCallNode(
         new BracedExpressionNode(new InstanceCreationNode(array(
           'type'           => new TypeName('Date'),
           'parameters'     => null,
         ))),
         'toString',
         null
-      )), 
+      )], 
       $this->parse('(new Date())->toString();')
     );
   }
@@ -52,13 +53,13 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function arrayOffsetOnMethod() {
     $this->assertEquals(
-      array(new MemberAccessNode(
+      [new MemberAccessNode(
         new ArrayAccessNode(
           new MethodCallNode(new VariableNode('l'), 'elements', null),
           new IntegerNode('0')
         ),
         'name'
-      )),
+      )],
       $this->parse('$l->elements()[0]->name;')
     );
   }
@@ -66,11 +67,11 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function chainedAfterStaticMethod() {
     $this->assertEquals(
-      array(new MethodCallNode(
-        new StaticMethodCallNode(new TypeName('Logger'), 'getInstance', array()),
+      [new MethodCallNode(
+        new StaticMethodCallNode(new TypeName('Logger'), 'getInstance', []),
         'configure', 
-        array(new StringNode('etc'))
-      )), 
+        [new StringNode('etc')]
+      )], 
       $this->parse('Logger::getInstance()->configure("etc");')
     );
   }
@@ -78,11 +79,11 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function chainedAfterFunction() {
     $this->assertEquals(
-      array(new MethodCallNode(
-        new InvocationNode('create', array(new VariableNode('a'))),
+      [new MethodCallNode(
+        new InvocationNode('create', [new VariableNode('a')]),
         'equals',
-        array(new VariableNode('b'))
-      )), 
+        [new VariableNode('b')]
+      )], 
       $this->parse('create($a)->equals($b);')
     );
   }
@@ -90,12 +91,34 @@ class ChainingTest extends ParserTestCase {
   #[@test]
   public function chainedAfterBraced() {
     $this->assertEquals(
-      array(new MethodCallNode(
+      [new MethodCallNode(
         new BracedExpressionNode(new VariableNode('a')),
         'equals', 
-        array(new VariableNode('b'))
-      )), 
+        [new VariableNode('b')]
+      )], 
       $this->parse('($a)->equals($b);')
+    );
+  }
+
+  #[@test]
+  public function arrayDereferencing() {
+    $this->assertEquals(
+      [new ArrayAccessNode(
+        new ArrayNode(['values' => [new IntegerNode('1'), new IntegerNode('2'), new IntegerNode('3')]]),
+        new IntegerNode('0')
+      )],
+      $this->parse('array(1, 2, 3)[0];')
+    );
+  }
+
+  #[@test]
+  public function stringDereferencing() {
+    $this->assertEquals(
+      [new ArrayAccessNode(
+        new StringNode('Hello'),
+        new IntegerNode('0')
+      )],
+      $this->parse('"Hello"[0];')
     );
   }
 }
