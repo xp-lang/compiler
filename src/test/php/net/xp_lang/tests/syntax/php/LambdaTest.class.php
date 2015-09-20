9c351be4137a934a02708c426ca63c2000fab31f
Timm Friebe <thekid@thekid.de>
2015-09-20 16:26:17 +0200

    Make capturing explicit when using the PHP lambda form

--- src/test/php/net/xp_lang/tests/syntax/php/LambdaTest.class.php
+++ src/test/php/net/xp_lang/tests/syntax/php/LambdaTest.class.php
@@ -7,31 +7,20 @@ use xp\compiler\ast\VariableNode;
 use xp\compiler\ast\IntegerNode;
 use xp\compiler\ast\BinaryOpNode;
 
-/**
- * TestCase
- *
- */
 class LambdaTest extends ParserTestCase {
 
-  /**
-   * Test simple lambda
-   *
-   */
   #[@test]
   public function noParameters() {
     $this->assertEquals(
       array(new LambdaNode(
         array(),
-        array(new ReturnNode(new BooleanNode(true)))
+        array(new ReturnNode(new BooleanNode(true))),
+        array()
       )), 
       $this->parse('function() { return true; };')
     );
   }
 
-  /**
-   * Test simple lambda
-   *
-   */
   #[@test]
   public function oneParameter() {
     $this->assertEquals(
@@ -41,16 +30,13 @@ class LambdaTest extends ParserTestCase {
           'lhs' => new VariableNode('a'),
           'rhs' => new IntegerNode('1'),
           'op'  => '+'
-        ))))
+        )))),
+        array()
       )), 
       $this->parse('function($a) { return $a + 1; };')
     );
   }
 
-  /**
-   * Test simple lambda
-   *
-   */
   #[@test]
   public function twoParameters() {
     $this->assertEquals(
@@ -60,9 +46,22 @@ class LambdaTest extends ParserTestCase {
           'lhs' => new VariableNode('a'),
           'rhs' => new VariableNode('b'),
           'op'  => '+'
-        ))))
+        )))),
+        array()
       )), 
       $this->parse('function($a, $b) { return $a + $b; };')
+    );
+  }
+
+  #[@test]
+  public function withUses() {
+    $this->assertEquals(
+      array(new LambdaNode(
+        array(new VariableNode('a')),
+        array(/* TBI */),
+        array(new VariableNode('mul'), new VariableNode('neg'))
+      )), 
+      $this->parse('function($a) use($mul, $neg) { /* TBI */ };')
     );
   }
 }
